7392c4f552ffe058342996425272e531a351c6fe
malikalichsan <malik.al.ichsan@gmail.com>
2021-01-06 01:26:10 +0700

    WIP for book resource

--- tests/Feature/BookManagementTest.php
+++ tests/Feature/BookManagementTest.php
@@ -6,7 +6,7 @@ use App\Book;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class BookReservationTest extends TestCase
+class BookManagementTest extends TestCase
 {
     use RefreshDatabase;
 
@@ -15,15 +15,17 @@ class BookReservationTest extends TestCase
     {
         // jika ingin liat balikan error nya bisa pakai ini,
         // seperti ingin ngedebug
-        $this->withoutExceptionHandling();
+//        $this->withoutExceptionHandling();
 
         $response = $this->post('/books', [
             'title' => 'Cool book title',
             'author' => 'Victor'
         ]);
 
-        $response->assertOk();
+        $book = Book::first();
+
         $this->assertCount(1, Book::all());
+        $response->assertRedirect($book->path());
     }
 
     /** @test */
@@ -51,7 +53,7 @@ class BookReservationTest extends TestCase
     /** @test */
     public function a_book_can_be_updated()
     {
-        $this->withoutExceptionHandling();
+//        $this->withoutExceptionHandling();
 
         $this->post('/books', [
             'title' => 'Cool Title of Book',
@@ -60,12 +62,32 @@ class BookReservationTest extends TestCase
 
         $book = Book::first();
 
-        $response = $this->patch('/books/' . $book->id, [
+        $response = $this->patch($book->path(), [
             'title' => 'New Title',
             'author' => 'New Author'
         ]);
 
         $this->assertEquals('New Title', Book::first()->title);
         $this->assertEquals('New Author', Book::first()->author);
+        $response->assertRedirect($book->fresh()->path());
+    }
+
+    /** @test */
+    public function a_book_can_be_deleted()
+    {
+//        $this->withoutExceptionHandling();
+
+        $this->post('/books', [
+            'title' => 'Cool Title of Book',
+            'author' => 'Victor'
+        ]);
+
+        $book = Book::first();
+        $this->assertCount(1, Book::all());
+
+        $response = $this->delete($book->path());
+
+        $this->assertCount(0, Book::all());
+        $response->assertRedirect('/books');
     }
 }
